bdbae679f320d2eee1913a66513c1780d01bb814
Guillaume de Rybel <90607561+gderybel@users.noreply.github.com>
2023-03-16 14:38:44 +0100

    Feature/add remediation (#6)
    * Add remediation to scrapped data
    * Add BL (Bitlocker Level) to possible levels
    * Remove remediation comments

--- cis_pdf_scrapper.py
+++ cis_pdf_scrapper.py
@@ -22,11 +22,11 @@ class CISPdfScrapper:
     def ParsePolicyName(self, policy_name):
         # Get level 
         final_level = ""
-        possible_levels = ['(L1)','(L2)','(NG)']
+        possible_levels = ['(L1)','(L2)','(NG)', '(BL)']
         for level in possible_levels:
             if level in policy_name:
-                final_level = level 
-        return final_level 
+                final_level = level
+        return final_level
 
     """
         This function will identify the order of the different paragraphs.
@@ -86,7 +86,7 @@ class CISPdfScrapper:
         # Add csv header to csv output
         try:
             f = open(self.output_filepath, 'w+')
-            f.write('"ID","Level","Policy Name","Default Value","Recommended Value","Impact","Description","Rationale"\n')
+            f.write('"ID","Level","Policy Name","Default Value","Recommended Value","Impact","Description","Rationale","Remediation"\n')
             f.close()
         except:
             throw("Couldn't write to output filepath, please verify you have rights to write, exiting.", "highs")
@@ -153,7 +153,16 @@ class CISPdfScrapper:
                     next_val = r'\n(.*)'
                 else:
                     next_val = sorted_[remediation_index+1]
-                #remediation_content = re.findall(r'Remediation:\n((.|\n)*?)'+next_val, policy)[0][0].replace('\n','').replace("\"","\'") # Retreive remediation
+                out =[]
+                strings = policy.splitlines()
+                for index, line in enumerate(strings):
+                    if 'Computer Configuration\\' in line or 'User Configuration\\' in line:
+                        if index+1 < len(strings) and '\\' in strings[index+1]:
+                            line+=strings[index+1]
+                        out.append(line.strip())
+                if len(out)==0:
+                    print(policy)
+                remediation_content = ';'.join(out)
 
             if impact:
                 impact_index = sorted_.index('Impact:')
@@ -188,7 +197,7 @@ class CISPdfScrapper:
             level = self.ParsePolicyName(policy_name)
 
             f = open(self.output_filepath, 'a')
-            f.write('"'+id+'","'+level+'","'+policy_name+'","'+defaultvalue_content+'","'+recommended_value+'","'+impact_content+'","'+description_content+'","'+rationale_content+'"\n')
+            f.write('"'+id+'","'+level+'","'+policy_name+'","'+defaultvalue_content+'","'+recommended_value+'","'+impact_content+'","'+description_content+'","'+rationale_content+'","'+remediation_content+'"\n')
             f.close()
 
         
